27275f649400e647238648d66f52a0157826a9ee
foxthefox <foxthefox@wysiwis.net>
2022-01-04 13:27:12 +0100

    2.0.1

--- example.js
+++ example.js
@@ -1,35 +1,51 @@
-var YamahaYXC = require("./yamahayxc.js");
-var yamaha = new YamahaYXC();
+var YamahaYXC = require('./yxc_api_cmd.js');
 
-yamaha.discover().then(function(result){
- 	console.log(result);
-     console.log('IP' + result[0]);
-});
+async function discover() {
+	var yamaha = new YamahaYXC();
+	try {
+		const result = await yamaha.discover(2000);
+		console.log(result);
+		console.log('IP' + result[0].ip);
+		yamaha.getXML('http://192.168.178.26:49154/MediaRenderer/desc.xml').then((result) => {
+			console.log('getXML', result);
+		});
+	} catch (error) {
+		return Promise.reject(error);
+	}
+}
 
+//var yamaha1 = new YamahaYXC('192.168.178.26');
+//var yamaha1 = new YamahaYXC();
+var yamaha1 = new YamahaYXC('localhost:3311');
 
+async function test() {
+	try {
+		const result1 = await yamaha1.getDeviceInfo();
+		console.log(result1);
+		var att = result1;
+		console.log('dest  ' + att.destination);
+		console.log('api  ' + att.api_version);
+	} catch (error) {
+		return Promise.reject(error);
+	}
 
-var yamaha1 = new YamahaYXC("192.168.178.52");
-
-yamaha1.getDeviceInfo().done(function(result){
- 	console.log(result);
-     var att = JSON.parse(result);
-     console.log('dest  ' + att.destination);
-     console.log('api  ' + att.api_version);
-});
-
-
-yamaha1.getFeatures().done(function(result){
- 	console.log(result);
-     console.log(result.response_code);
-     var att = JSON.parse(result);
-     console.log(att.response_code);
-     console.log(JSON.parse(result).response_code);
-     console.log('dest  ' + att.system.zone_num);
-     console.log('api  ' + att.system.func_list);
-     console.log('func list  ' + JSON.stringify(att.zone[0].func_list));
-     console.log('input list  ' + JSON.stringify(att.system.input_list[0]));
-     console.log('input 1  ' + att.system.input_list[0].id);
-     console.log('link list  ' + JSON.stringify(att.zone[0].link_control_list));
-     console.log('sound list  ' + JSON.stringify(att.zone[0].sound_program_list));
-     console.log('input zone list  ' + JSON.stringify(att.zone[0].input_list));
-});
+	try {
+		const result = await yamaha1.getFeatures();
+		console.log(result);
+		var att = result;
+		console.log(att.response_code);
+		console.log(result.response_code);
+		console.log('dest  ' + att.system.zone_num);
+		console.log('api  ' + att.system.func_list);
+		console.log('func list  ' + JSON.stringify(att.zone[0].func_list));
+		console.log('input list  ' + JSON.stringify(att.system.input_list[0]));
+		console.log('input 1  ' + att.system.input_list[0].id);
+		console.log('link list  ' + JSON.stringify(att.zone[0].link_control_list));
+		console.log('sound list  ' + JSON.stringify(att.zone[0].sound_program_list));
+		console.log('input zone list  ' + JSON.stringify(att.zone[0].input_list));
+	} catch (error) {
+		return Promise.reject(error);
+	}
+}
+test();
+//discover();
