eb57ce46142d7a8df8354cdacff59c0db2edb09c
foxthefox <foxthefox@wysiwis.net>
2025-11-02 16:23:35 +0100

    3.2.0

--- discover_get.js
+++ discover_get.js
@@ -1,96 +1,97 @@
-import { YamahaYXC } from './index.js';
+import { YamahaYXC } from "./index.js";
 // import { YamahaYXC } from 'yamaha-yxc-nodejs';
 
 async function discover() {
-	var yamaha = new YamahaYXC();
-	try {
-		const result = await yamaha.discover();
-		console.table(result);
-		return Promise.resolve(result);
-	} catch (error) {
-		return Promise.reject(error);
-	}
+  var yamaha = new YamahaYXC();
+  try {
+    const result = await yamaha.discover();
+    console.table(result);
+    return Promise.resolve(result);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 
 async function discoverAndGet() {
-	let found = [];
-	try {
-		const devicearray = await discover();
-		if (devicearray) {
-			await Promise.all(
-				devicearray.map(async (device) => {
-					let data = {};
-					data[device.name] = {};
-					const yamaha = new YamahaYXC(device.ip);
-					data[device.name]['system'] = {};
-					const getDeviceInfo = await yamaha['getDeviceInfo']();
-					data[device.name]['system']['getDeviceInfo'] = getDeviceInfo;
-					const getNetworkStatus = await yamaha.getNetworkStatus();
-					data[device.name]['system']['getNetworkStatus'] = getNetworkStatus;
-					const getFuncStatus = await yamaha.getFuncStatus();
-					data[device.name]['system']['getFuncStatus'] = getFuncStatus;
-					const getLocationInfo = await yamaha.getLocationInfo();
-					data[device.name]['system']['getLocationInfo'] = getLocationInfo;
-					const getFeatures = await yamaha.getFeatures();
-					data[device.name]['system']['getFeatures'] = getFeatures;
-					data[device.name]['dist'] = {};
-					const getDistributionInfo = await yamaha.getDistributionInfo();
-					data[device.name]['dist']['getFeatures'] = getDistributionInfo;
-					if (getFeatures['netusb']) {
-						data[device.name]['netusb'] = {};
-						const getNetPlayInfo = await yamaha.getPlayInfo();
-						data[device.name]['netusb']['getPlayInfo'] = getNetPlayInfo;
-						const getPresetInfo = await yamaha.getPresetInfo();
-						data[device.name]['netusb']['getPresetInfo'] = getPresetInfo;
-						const getSettings = await yamaha.getSettings();
-						data[device.name]['netusb']['getSettings'] = getSettings;
-						const getRecentInfo = await yamaha.getRecentInfo();
-						data[device.name]['netusb']['getRecentInfo'] = getRecentInfo;
-					}
-					if (getFeatures['tuner']) {
-						data[device.name]['tuner'] = {};
-						const getTunerPlayInfo = await yamaha.getTunerPlayInfo();
-						data[device.name]['tuner']['getPlayInfo'] = getTunerPlayInfo;
-						const getTunerPresetInfo = await yamaha.getTunerPresetInfo();
-						data[device.name]['tuner']['getPresetInfo'] = getTunerPresetInfo;
-					}
-					if (getFeatures['cd']) {
-						data[device.name]['cd'] = {};
-						const getCdPlayInfo = await yamaha.getPlayInfo('cd');
-						data[device.name]['cd']['getPlayInfo'] = getCdPlayInfo;
-					}
-					if (getFeatures['clock']) {
-						data[device.name]['clock'] = {};
-						const getClockSettings = await yamaha.getClockSettings();
-						data[device.name]['clock']['getSettings'] = getClockSettings;
-					}
-					if (getFeatures['zone']) {
-						await Promise.all(
-							getFeatures['zone'].map(async (zone) => {
-								data[device.name][zone.id] = {};
-								const getStatus = await yamaha.getStatus();
-								data[device.name][zone.id]['getStatus'] = getStatus;
-								const getSoundProgramList = await yamaha.getSoundProgramList();
-								data[device.name][zone.id]['getSoundProgramList'] = getSoundProgramList;
-								const getSignalInfo = await yamaha.getSoundProgramList();
-								data[device.name][zone.id]['getSignalInfo'] = getSignalInfo;
-							})
-						);
-					}
+  const found = [];
+  try {
+    const devicearray = await discover();
+    if (devicearray) {
+      await Promise.all(
+        devicearray.map(async (device) => {
+          const data = {};
+          data[device.name] = {};
+          const yamaha = new YamahaYXC(device.ip);
+          data[device.name]["system"] = {};
+          const getDeviceInfo = await yamaha["getDeviceInfo"]();
+          data[device.name]["system"]["getDeviceInfo"] = getDeviceInfo;
+          const getNetworkStatus = await yamaha.getNetworkStatus();
+          data[device.name]["system"]["getNetworkStatus"] = getNetworkStatus;
+          const getFuncStatus = await yamaha.getFuncStatus();
+          data[device.name]["system"]["getFuncStatus"] = getFuncStatus;
+          const getLocationInfo = await yamaha.getLocationInfo();
+          data[device.name]["system"]["getLocationInfo"] = getLocationInfo;
+          const getFeatures = await yamaha.getFeatures();
+          data[device.name]["system"]["getFeatures"] = getFeatures;
+          data[device.name]["dist"] = {};
+          const getDistributionInfo = await yamaha.getDistributionInfo();
+          data[device.name]["dist"]["getFeatures"] = getDistributionInfo;
+          if (getFeatures["netusb"]) {
+            data[device.name]["netusb"] = {};
+            const getNetPlayInfo = await yamaha.getPlayInfo();
+            data[device.name]["netusb"]["getPlayInfo"] = getNetPlayInfo;
+            const getPresetInfo = await yamaha.getPresetInfo();
+            data[device.name]["netusb"]["getPresetInfo"] = getPresetInfo;
+            const getSettings = await yamaha.getSettings();
+            data[device.name]["netusb"]["getSettings"] = getSettings;
+            const getRecentInfo = await yamaha.getRecentInfo();
+            data[device.name]["netusb"]["getRecentInfo"] = getRecentInfo;
+          }
+          if (getFeatures["tuner"]) {
+            data[device.name]["tuner"] = {};
+            const getTunerPlayInfo = await yamaha.getTunerPlayInfo();
+            data[device.name]["tuner"]["getPlayInfo"] = getTunerPlayInfo;
+            const getTunerPresetInfo = await yamaha.getTunerPresetInfo();
+            data[device.name]["tuner"]["getPresetInfo"] = getTunerPresetInfo;
+          }
+          if (getFeatures["cd"]) {
+            data[device.name]["cd"] = {};
+            const getCdPlayInfo = await yamaha.getPlayInfo("cd");
+            data[device.name]["cd"]["getPlayInfo"] = getCdPlayInfo;
+          }
+          if (getFeatures["clock"]) {
+            data[device.name]["clock"] = {};
+            const getClockSettings = await yamaha.getClockSettings();
+            data[device.name]["clock"]["getSettings"] = getClockSettings;
+          }
+          if (getFeatures["zone"]) {
+            await Promise.all(
+              getFeatures["zone"].map(async (zone) => {
+                data[device.name][zone.id] = {};
+                const getStatus = await yamaha.getStatus();
+                data[device.name][zone.id]["getStatus"] = getStatus;
+                const getSoundProgramList = await yamaha.getSoundProgramList();
+                data[device.name][zone.id]["getSoundProgramList"] =
+                  getSoundProgramList;
+                const getSignalInfo = await yamaha.getSoundProgramList();
+                data[device.name][zone.id]["getSignalInfo"] = getSignalInfo;
+              })
+            );
+          }
 
-					found.push(data);
-				})
-			);
-			return Promise.resolve(found);
-		}
-	} catch (error) {
-		return Promise.reject(error);
-	}
+          found.push(data);
+        })
+      );
+      return Promise.resolve(found);
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 
 discoverAndGet().then((res) => {
-	console.table(res);
-	console.log(res[0]);
-	console.log(JSON.stringify(res[0]));
-	console.log(JSON.stringify(res[1]));
+  console.table(res);
+  console.log(res[0]);
+  console.log(JSON.stringify(res[0]));
+  console.log(JSON.stringify(res[1]));
 });
--- test/integration.js
+++ test/integration.js
@@ -1,51 +1,53 @@
 // import assert from 'assert';
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
-const YamahaYXC = require('../index.js').YamahaYXC;
-const YamahaYXCEmu = require('../index.js').YamahaYXCEmu;
+const YamahaYXC = require("../index.js").YamahaYXC;
+const YamahaYXCEmu = require("../index.js").YamahaYXCEmu;
 
-const fs = require('fs');
-const path = require('path');
-console.log('PATH ist ' + path.join(__dirname, './data/'));
+const fs = require("fs");
+const path = require("path");
+console.log("PATH ist " + path.join(__dirname, "./data/"));
 
-let testfile = 'YSP1600_312_208.json';
-let testdevice = 'YSP-1600';
-const YXCresponses = fs.readFileSync(path.join(__dirname, './data/') + testfile);
+const testfile = "YSP1600_312_208.json";
+const testdevice = "YSP-1600";
+const YXCresponses = fs.readFileSync(
+  path.join(__dirname, "./data/") + testfile
+);
 const deviceresp = JSON.parse(String(YXCresponses))[testdevice];
 
 /*Tests*/
-describe('Integration Test of Musiccast-API', () => {
-	let port = 3311;
-	before('start the Musiccast emulation', () => {
-		const emulation = new YamahaYXCEmu(deviceresp, port, false);
-		emulation.setupHttpServer(function() {});
-	});
-	// if promise is returned = success
-	var yamaha;
-	it('should create a new yxc musiccast instance', function() {
-		yamaha = new YamahaYXC('localhost:' + port, null);
-	});
-	it('read deviceinfo', async () => {
-		const result = await yamaha.getDeviceInfo();
-		// console.log('result', result);
-		expect(result.response_code).to.equal(0);
-		expect(result.model_name).to.equal('YSP-1600');
-		expect(result.destination).to.equal('BG');
-		expect(result.device_id).to.equal('00A0DED15025');
-		expect(result.system_id).to.equal('0B587073');
-		expect(result.system_version).to.equal(3.12);
-		expect(result.api_version).to.equal(2.08);
-		expect(result.netmodule_generation).to.equal(1);
-		expect(result.netmodule_version).to.equal('1924    ');
-		expect(result.netmodule_checksum).to.equal('EAD51507');
-		expect(result.operation_mode).to.equal('normal');
-		expect(result.update_error_code).to.equal('00000000');
-	});
-	it('check something stupid', async () => {
-		const result = await yamaha.getSignalInfo(5);
-		// console.log('result', result);
-		expect(result.response_code).to.equal(3);
-	});
+describe("Integration Test of Musiccast-API", () => {
+  const port = 3311;
+  before("start the Musiccast emulation", () => {
+    const emulation = new YamahaYXCEmu(deviceresp, port, false);
+    emulation.setupHttpServer(function () {});
+  });
+  // if promise is returned = success
+  var yamaha;
+  it("should create a new yxc musiccast instance", function () {
+    yamaha = new YamahaYXC("localhost:" + port, null);
+  });
+  it("read deviceinfo", async () => {
+    const result = await yamaha.getDeviceInfo();
+    // console.log('result', result);
+    expect(result.response_code).to.equal(0);
+    expect(result.model_name).to.equal("YSP-1600");
+    expect(result.destination).to.equal("BG");
+    expect(result.device_id).to.equal("00A0DED15025");
+    expect(result.system_id).to.equal("0B587073");
+    expect(result.system_version).to.equal(3.12);
+    expect(result.api_version).to.equal(2.08);
+    expect(result.netmodule_generation).to.equal(1);
+    expect(result.netmodule_version).to.equal("1924    ");
+    expect(result.netmodule_checksum).to.equal("EAD51507");
+    expect(result.operation_mode).to.equal("normal");
+    expect(result.update_error_code).to.equal("00000000");
+  });
+  it("check something stupid", async () => {
+    const result = await yamaha.getSignalInfo(5);
+    // console.log('result', result);
+    expect(result.response_code).to.equal(3);
+  });
 });
 
 /*
